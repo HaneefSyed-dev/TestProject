cfe73444d51e0cb906c339a8ca9560bdef89404d
HaneefSyed-dev <haneef.syed@optum.com>
2021-03-03 12:14:51 -0500

    f

--- src/com/test/test1.java
+++ src/com/test/test1.java
@@ -3,15 +3,6 @@ package com.test;
 public class test1 {
 	
 	public static void main(String[] args) {
-		System.out.println("git demo");
-		
-		
-		System.out.println("Updated Code after downloading from GIT");
-		
-		System.out.println("New Stuff");
-		
-		System.out.println("DEVElop branch");
-		System.out.println("finally");
 		
 		
 	}
